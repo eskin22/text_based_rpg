d83f07e742516d48dd44b461de2e3058fcc24b95
eskin22 <100242871+eskin22@users.noreply.github.com>
2022-07-09 23:12:56 -0500

    Update levelOneRooms.py

--- rooms/levelOneRooms.py
+++ rooms/levelOneRooms.py
@@ -2,6 +2,21 @@
 class Square():
     def __init__(self):
         self.name = None
-        self.chest = None
+        
+class ItemSquare(Square):
+    def __init__(self):
+        super().__init__()
+        self.name = "Item Square"
         self.item = None
+
+class EnemySquare(Square):
+    def __init__(self):
+        super().__init__()
+        self.name = "Enemy Square"
         self.enemy = None
+
+class ChestSquare(Square):
+    def __init__(self):
+        super().__init__()
+        self.name = "Chest Square"
+        self.chest = None
